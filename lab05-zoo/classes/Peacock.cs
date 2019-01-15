fb847b4d29d7d19e62b4f2ea3a3f50402621442e
juliely8619 <july8619@yahoo.com>
2019-01-15 15:05:36 -0800

    forgot peacock class, done now.

--- lab05-zoo/classes/Peacock.cs
+++ lab05-zoo/classes/Peacock.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace lab05_zoo.classes
 {
-    class Peacock
+    class Peacock : Bird
     {
+        public override bool HasFeathers { get; set; } = true;
+        public override int NumLegs { get; set; } = 2;
+        public string MainColorOfTail { get; set; }
+        public override void Move()
+        {
+            Console.WriteLine("Move in peacock class");
+        }
+        public override void Speak()
+        {
+            Console.WriteLine("Speak in peacock class");
+        }
+        public override void Sleep()
+        {
+            Console.WriteLine("Sleep in peacock class");
+        }
     }
 }

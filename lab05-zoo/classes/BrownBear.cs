457abd9fcbaba20bb6d3c3cc9a2fe73d39210b0c
juliely8619 <july8619@yahoo.com>
2019-01-15 13:44:05 -0800

    brownbear class done

--- lab05-zoo/classes/BrownBear.cs
+++ lab05-zoo/classes/BrownBear.cs
@@ -4,7 +4,29 @@ using System.Text;
 
 namespace lab05_zoo.classes
 {
-    class BrownBear
+    public class BrownBear : Mammal
     {
+        public override string FurColor { get; set; } = "brown";
+        public override int NumLegs { get; set; } = 2;
+        public override void Move()
+        {
+            Console.WriteLine("Move in brownbear class");
+        }
+        public override void Speak()
+        {
+            Console.WriteLine("Speak in brownbear class");
+        }
+        public override void Sleep()
+        {
+            Console.WriteLine("Sleep in brownbear class");
+        }
+        public override void Births()
+        {
+            Console.WriteLine("Births in brownbear class");
+        }
+        public void EatHoney()
+        {
+            Console.WriteLine("EatHoney in brownbear class");
+        }
     }
 }
